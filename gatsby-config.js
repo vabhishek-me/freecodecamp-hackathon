67b247948f64a2c0fcd823663d34641f612aad3b
vabhishek-me <abhishek.k.v76076@gmail.com>
2018-09-22 15:59:35 +0530

    updated gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,19 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Freecodecamp Hackathon Submission',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
+        name: 'freecodecamp-hackathon-submission',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#006400',
+        theme_color: '#006400',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/freecodecamp_logo.svg', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-offline',
